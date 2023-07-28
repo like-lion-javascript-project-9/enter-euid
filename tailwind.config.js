e4d40c4c7b505488399430f11ace280e915d2db5
cfgop23 <cfgop23@gmail.com>
2023-07-28 20:58:24 +0900

    setting : 프로젝트 초기 세팅, template.html 생성, tailwind.config.js 추가(#11)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./client/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        search: "url('../assets/icons/search.svg')",
+      }),
+      colors: {
+        white: "#ffffff",
+        black: "#000000",
+        gray1: "#333333",
+        gray2: "#4f4f4f",
+        gray3: "#828282",
+        gray4: "#bdbdbd",
+        gray5: "#e0e0e0",
+        gray6: "#f2f2f2",
+        gray7: "#f9f9f9",
+        primary: "#373F67",
+        secondary: "#5A85EE",
+        tertiary: "#719CF7",
+      },
+      fontFamily: {
+        pretendard: ["pretendard"],
+      },
+      fontWeight: {
+        thin: 100,
+        normal: 400,
+        bold: 600,
+      },
+    },
   },
   plugins: [],
 };
