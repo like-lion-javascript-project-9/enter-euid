c861c8d816ab66e973da574edba6e24344797298
Suwan <imissyou5466@gmail.com>
2023-07-29 22:47:54 +0900

    [error] dom/lib/index.js 내부 불필요한 파일 제거 및 data.json 오탈자 수정

---
+++ client/js/maindetail/main.js
@@ -0,0 +1,17 @@
+import { getNode, insertLast, tiger } from "./../lib/index.js";
+
+const URL = "http://localhost:3000/products";
+
+const response = await tiger.get(URL);
+const itemlist = response.data;
+console.log(itemlist);
+const productWrapper = getNode(".productWrapper");
+// const template = `
+// <img class="swiper-slide product__item1" src="${itemlist[0].image["thumbnai_l"]}"></img>
+// <img class="swiper-slide product__item2"></img>
+// `;
+
+const render = () => {
+  insertLast(productWrapper);
+};
+render();
--- client/js/maindetail/product/swiper.js
+++
@@ -1,4 +0,0 @@
-import Swiper from "./swiper.js";
-
-export const swiper = new Swiper(".swiper", {});
-console.log(swiper);
