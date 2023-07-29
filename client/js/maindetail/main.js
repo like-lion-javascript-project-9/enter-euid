9def85fc1e23b4d991f979dffd2300233df42af6
Suwan <imissyou5466@gmail.com>
2023-07-29 23:04:08 +0900

    [modify] data.json 오탈자 수정

--- client/js/maindetail/main.js
+++ client/js/maindetail/main.js
@@ -6,12 +6,12 @@ const response = await tiger.get(URL);
 const itemlist = response.data;
 console.log(itemlist);
 const productWrapper = getNode(".productWrapper");
-// const template = `
-// <img class="swiper-slide product__item1" src="${itemlist[0].image["thumbnai_l"]}"></img>
-// <img class="swiper-slide product__item2"></img>
-// `;
+const template = `
+<img class="swiper-slide product__item1" src="${itemlist[0].image["thumbnai_l"]}"></img>
+<img class="swiper-slide product__item2"></img>
+`;
 
 const render = () => {
-  insertLast(productWrapper);
+  insertLast(productWrapper, template);
 };
 render();
