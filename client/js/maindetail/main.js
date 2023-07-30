471b7d7aec9b83bd5ba36754982c39c36a8cd94b
Suwan <imissyou5466@gmail.com>
2023-07-30 16:59:37 +0900

    [core] data.json 파일 asseets 경로 수정

--- client/js/maindetail/main.js
+++ client/js/maindetail/main.js
@@ -1,17 +1,35 @@
-import { getNode, insertLast, tiger } from "./../lib/index.js";
+import {
+  getNode,
+  getRandom,
+  insertAfter,
+  insertLast,
+  tiger,
+} from "./../lib/index.js";
 
 const URL = "http://localhost:3000/products";
 
 const response = await tiger.get(URL);
 const itemlist = response.data;
 console.log(itemlist);
+const rnadomIdx = getRandom(itemlist.length - 1);
 const productWrapper = getNode(".productWrapper");
-const template = `
-<img class="swiper-slide product__item1" src="${itemlist[0].image["thumbnai_l"]}"></img>
-<img class="swiper-slide product__item2"></img>
+const productDetail__title = getNode(".productDetail__title");
+const detailTemplate = `
+        <h2 class="productDetail__title pb-1 text-lg font-bold">
+        ${itemlist[rnadomIdx].name}
+        </h2>
+`;
+
+const itemTemp = `
+<img class="swiper-slide product__item1" src="${itemlist[rnadomIdx].image.thumbnail_2}" alt=${itemlist[0].image.alt}></div>
 `;
 
 const render = () => {
-  insertLast(productWrapper, template);
+  insertAfter(productDetail__title, detailTemplate);
+  insertLast(productWrapper, itemTemp);
 };
 render();
+
+// ${itemlist[rnadomIdx].name}
+
+// ${itemlist[rnadomIdx].image.thumbnail_2}
