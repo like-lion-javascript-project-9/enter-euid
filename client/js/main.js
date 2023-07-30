d7ab73758bc6e73f3e4af7728c3da01bcf38c58c
leesy <leesy787@gmail.com>
2023-07-31 04:27:58 +0900

    [modify]  index.html 수정 tailwind.css 수정 category.html 수정  main.js작성 category.js 작성

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,13 @@
-import {} from "./layout/index.js";
+
+import { getNode } from "./lib/index.js";
+
+const startButton = getNode(".startButton");
+
+const handliClick = event => {
+event.preventDefault();
+window.location.href="http://localhost:5500/views/category.html";
+}
+startButton.addEventListener("click", handliClick);
+
+
+
