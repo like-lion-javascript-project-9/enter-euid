c58d153f2d481c7e6d3d8b2db4d4a448510c341e
leesy <leesy787@gmail.com>
2023-08-02 01:39:21 +0900

    [modify] search.js 수정

--- client/js/search.js
+++ client/js/search.js
@@ -1,3 +1,4 @@
+
 import { renderPhoneIndicator } from './layout/index.js';
 import { debounce, getNode, insertLast } from './lib/index.js';
 import { getProductList } from './maindetail/async.js';
@@ -8,16 +9,16 @@ const container = getNode('#container');
 
 const createItem = (obj) => {
   const template = `
-    <li class="product__list">
-    <a href="maindetail.html" class="list w-36">
-      <figure class="figure">
+    <li class="product__list p-4 border-t border-b">
+    <a href="maindetail.html" class="list">
+      <figure class="figure flex">
         <img
         src="/${obj.image.thumbnail_l}.webp"
         alt="/${obj.image.alt}"
-        class="image h-24 w-full rounded object-cover"
+        class="image h-16 w-16 rounded object-cover mt-4"
         />
-        <figcaption class="list">
-          <h1 class="list-name text-xs py-1">${obj.name}</h1>
+        <figcaption class="list inline-block ml-4 mt-2">
+          <h1 class="list-name text-xs py-1 font-bold">${obj.name}</h1>
           <p class="list-location text-xs text-gray-500">${obj.user.address}</p>
           <strong class="list-price font-bold text-xs">${obj.price}원</strong>
         </figcaption>
@@ -28,58 +29,14 @@ const createItem = (obj) => {
   return template;
 };
 
-<<<<<<< HEAD
-import { getNode } from "./lib/index.js";
-// import { getProductList } from './maindetail/async';
-=======
 const resetUL = (insertNode, template) => {
   const productItem = getNode('.productWrapper');
   productItem.remove();
   insertLast(insertNode, template);
 };
->>>>>>> develop
 
 const ulTemplate = `<ul class="productWrapper"></ul>`;
 
-<<<<<<< HEAD
-
-
-const handleInputClick= () => {
-
-  // const productList = await getProductList();
-  // 필터로 데이더 이름
-
-}
-
-searchInput.addEventLIstner("input", handleInputClick);
-
-
-
-
-// debounce-------------------------------------------------
-function debounce(callback, limit = 100) {
-  let timeout;
-  return function (...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      callback.apply(this, args);
-    }, limit);
-  };
-}
-
-//2. thorottle
-
-function throttle(callback, limit = 100) {
-  let waiting = false;
-
-  return function () {
-    if (!waiting) {
-      callback.apply(this, arguments);
-      waiting = true;
-      setTimeout(() => {
-        waiting = false;
-      }, limit);
-=======
 const handleInput = debounce(async (e) => {
   e.preventDefault();
   resetUL(container, ulTemplate);
@@ -87,7 +44,6 @@ const handleInput = debounce(async (e) => {
   const searchArr = productList.filter((el) => {
     if (el.name.includes(inputText)) {
       return el;
->>>>>>> develop
     }
   });
   if (inputText === '') {
